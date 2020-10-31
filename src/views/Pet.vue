733be2b2e248a9d939f31c14212b3163b16800a5
Gabriel Dantas <gabrielmd123@gmail.com>
2020-10-31 11:00:09 -0300

    pet rout initial logic

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,9 +1,30 @@
 <template>
-  <h1>Pets view</h1>
+  <div class="pet">
+    <h1>{{ animal.name }}</h1>
+    <p>Age: {{ animal.age }}</p>
+    <p>Breed: {{ animal.breed }}</p>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  name: 'Pet'
+  name: 'Pet',
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animal = this[this.$route.params.spieces][this.$route.params.id]
+    this.animal = animal
+  }
 }
 </script>
